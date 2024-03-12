c997fc01b23971860f4ce00396f9914170acfcc9
joan-linc <52429936+joan-linc@users.noreply.github.com>
2024-03-12 00:38:02 +0000

    hide side bar

--- chat.py
+++ chat.py
@@ -5,9 +5,9 @@ from llama_index.embeddings.huggingface import HuggingFaceEmbedding
 from llama_index.core import Settings
 
 try:
-  from llama_index import VectorStoreIndex, ServiceContext, Document, SimpleDirectoryReader
+  from llama_index import VectorStoreIndex, ServiceContext, SimpleDirectoryReader
 except ImportError:
-  from llama_index.core import VectorStoreIndex, ServiceContext, Document, SimpleDirectoryReader
+  from llama_index.core import VectorStoreIndex, ServiceContext, SimpleDirectoryReader
 
 embed_model = HuggingFaceEmbedding(model_name="BAAI/bge-small-en-v1.5")
 Settings.embed_model = embed_model
